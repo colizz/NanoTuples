c61c17c62f30fe71d3ba407a189273df303a2d51
gaozutang <110015680+gaozutang@users.noreply.github.com>
2025-07-24 16:34:36 +0800

    Add DeepHgg

--- python/jetTools.py
+++ python/jetTools.py
@@ -845,8 +845,8 @@ def setupBTagging(process, jetSource, pfCandidates, explicitJTA, pvSource, svSou
                                       ),
                                     process, task)
 
-            ##!!! custom DeepHWW
-            if btagInfo in ['pfMassDecorrelatedDeepHWWV1TagInfos', 'pfMassDecorrelatedInclParticleTransformerV1TagInfos', 'pfMassDecorrelatedInclParticleTransformerV2TagInfos', 'pfMassDecorrelatedInclParticleTransformerAK15V2TagInfos']:
+            ##!!! custom DeepHWW and DeepHgg
+            if btagInfo in ['pfMassDecorrelatedDeepHWWV1TagInfos', 'pfMassDecorrelatedDeepHggV3TagInfos', 'pfMassDecorrelatedInclParticleTransformerV1TagInfos', 'pfMassDecorrelatedInclParticleTransformerV2TagInfos', 'pfMassDecorrelatedInclParticleTransformerAK15V2TagInfos']:
                 if pfCandidates.value() == 'packedPFCandidates':
                     # case 1: running over jets whose daughters are PackedCandidates (only via updateJetCollection for now)
                     vertex_associator = ""
